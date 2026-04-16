b88e816d46fc9cc99401e63e8ee6347bdaafbf24
Jaume Palau <jpalauserrano@gmail.com>
2026-04-16 16:54:52 +0200

    Boton Resolver renderizado en blanco y verde con button_hover

--- Python/Sudoku/selection.py
+++ Python/Sudoku/selection.py
@@ -19,13 +19,16 @@ class NumberSelection:
         self.number_list = list(range(1,10))
         self.selected_number = None
 
+        self.btn_resolution_coordinates = (950,800)
+        self.font_resolution = pg.font.SysFont('Arial',40)
+
 
     def draw(self,surface) -> None:
         """Dibuja los botones de seleccion"""
 
         for index, pos in enumerate(self.btn_coordinates):
 
-            if self.button_hover(pos):
+            if self.button_hover(pos,self.btn_w,self.btn_h):
 
                 pg.draw.rect(surface,self.selected_color,(pos[0],pos[1],self.btn_w,self.btn_h)
                             , width=1, border_radius=10)
@@ -47,6 +50,8 @@ class NumberSelection:
                     text_surface = self.font.render(str(self.number_list[index]),False,self.selected_color)    
 
             surface.blit(text_surface,(pos[0]+self.off_setx,pos[1]+self.off_sety))
+        
+        self.resolution_button(surface)
 
     
     def get_clicked_number(self, mouse_x: int, mouse_y: int):
@@ -54,19 +59,37 @@ class NumberSelection:
 
         for index, pos in enumerate(self.btn_coordinates):
 
-            if self.on_button(mouse_x,mouse_y,pos):
+            if self.on_button(mouse_x,mouse_y,pos,self.btn_w,self.btn_h):
                 self.selected_number = index + 1
         
 
-    def on_button(self, mouse_x: int, mouse_y: int, pos: tuple)-> bool:
+    def on_button(self, mouse_x: int, mouse_y: int, pos: tuple,width: int, height)-> bool:
         """Returns True if the given coordinates are inside the button"""
 
-        return pos[0] < mouse_x < pos[0]+self.btn_w and pos[1] < mouse_y < pos[1]+self.btn_h
+        return pos[0] < mouse_x < pos[0]+width and pos[1] < mouse_y < pos[1]+height
     
 
-    def button_hover(self, pos: tuple)-> bool:
+    def button_hover(self, pos: tuple,width,height)-> bool:
         """Returns True if the mouse is currently hovering over the button"""
 
         mouse_pos = pg.mouse.get_pos()
 
-        return self.on_button(mouse_pos[0],mouse_pos[1],pos)
+        return self.on_button(mouse_pos[0],mouse_pos[1],pos,width,height)
+    
+
+    def resolution_button(self,surface) -> None:
+
+        if self.button_hover(self.btn_resolution_coordinates,200,80):
+            pg.draw.rect(surface,self.selected_color,
+                (self.btn_resolution_coordinates[0],self.btn_resolution_coordinates[1],200,80),10,10)
+        
+            text_surface = self.font_resolution.render('Resolver',False,self.selected_color)
+        #surface.blit(text_surface,(self.btn_resolution_coordinates[0]+22,self.btn_resolution_coordinates[1]+20))           
+        
+        else:
+            pg.draw.rect(surface,self.normal_color,
+                (self.btn_resolution_coordinates[0],self.btn_resolution_coordinates[1],200,80),10,10)
+        
+            text_surface = self.font_resolution.render('Resolver',False,self.normal_color)
+
+        surface.blit(text_surface,(self.btn_resolution_coordinates[0]+22,self.btn_resolution_coordinates[1]+20))
